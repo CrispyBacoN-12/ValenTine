84283bf0b52b0924e46576ca5e2c890fa5054945
CrispyBacoN-12 <kannithifame2@gmail.com>
2026-01-22 19:25:49 +0700

    Update Authcontext.tsx

--- components/Authcontext.tsx
+++ components/Authcontext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { createContext, useContext, useEffect, useMemo, useState, type ReactNode } from "react";
-import { createClient } from "@/lib/supabaseClient";
+import { createClient } from "@/lib/supabase/client";
 
 interface AuthContextType {
   isLoading: boolean;
