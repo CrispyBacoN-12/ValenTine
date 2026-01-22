27b57376bbc7739fa4a6c2fc765c50ed7592ce8c
CrispyBacoN-12 <kannithifame2@gmail.com>
2026-01-22 19:17:09 +0700

    Create client.ts

---
+++ lib/supabase/client.ts
@@ -0,0 +1,13 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  const url =
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+  const anon =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+
+  if (!url) throw new Error("Missing SUPABASE URL");
+  if (!anon) throw new Error("Missing SUPABASE ANON KEY");
+
+  return createBrowserClient(url, anon);
+}
